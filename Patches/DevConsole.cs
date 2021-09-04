b7f1576f92a1907c133b97badb2f7e10a17b1f3f
tddebart <tddebart@gmail.com>
2021-09-04 16:08:39 +0200

    final changes before 1.0

--- Patches/DevConsole.cs
+++ Patches/DevConsole.cs
@@ -1,4 +1,6 @@
-﻿using HarmonyLib;
+﻿using System.Linq;
+using System.Reflection;
+using HarmonyLib;
 using Photon.Pun;
 using UnboundLib;
 using UnityEngine;
@@ -32,14 +34,44 @@ namespace BetterChat.Patches
         }
     }
 
+    [HarmonyPatch(typeof(DevConsole), "Send")]
+    class Patch_send_dev
+    {
+        private static bool Prefix(DevConsole __instance, string message)
+        {
+            if (string.IsNullOrWhiteSpace(message)) return false;
+            if (Application.isEditor || (GM_Test.instance && GM_Test.instance.gameObject.activeSelf))
+            {
+                typeof(DevConsole).InvokeMember("SpawnCard", BindingFlags.Instance | BindingFlags.InvokeMethod | BindingFlags.NonPublic, null, MenuControllerHandler.instance.GetComponent<DevConsole>(), new object[]{message});
+            }
+            if (Application.isEditor)
+            {
+                typeof(DevConsole).InvokeMember("SpawnMap", BindingFlags.Instance | BindingFlags.InvokeMethod | BindingFlags.NonPublic, null, MenuControllerHandler.instance.GetComponent<DevConsole>(), new object[]{message});
+            }
+            
+            var viewID = ((Player) typeof(PlayerManager).InvokeMember("GetPlayerWithActorID",
+                BindingFlags.Instance | BindingFlags.InvokeMethod | BindingFlags.NonPublic, null,
+                PlayerManager.instance, new object[] {PhotonNetwork.LocalPlayer.ActorNumber})).data.view.ViewID;
+            
+            __instance.GetComponent<PhotonView>().RPC("RPCA_SendChat", RpcTarget.Others, new object[]
+            {
+                "‌" + message,
+                viewID
+            });
+            return false;
+        }
+    }
+    
     [HarmonyPatch(typeof(DevConsole), "RPCA_SendChat")]
     class Patch_rpca_dev
     {
         private static bool Prefix(string message, int playerViewID)
         {
+            if (message.Contains("‌")) return false;
+            
             var player = PhotonNetwork.GetPhotonView(playerViewID);
             if (player.IsMine) return false;
-            MenuControllerHandler.instance.GetComponent<PhotonView>().RPC("RPCA_CreateMessage", RpcTarget.All, "Unmodded user", player.GetComponent<Player>().teamID, message);
+            MenuControllerHandler.instance.GetComponent<PhotonView>().RPC("RPCA_CreateMessage", RpcTarget.All, player.Owner.NickName, player.GetComponent<Player>().teamID, message);
             return false;
         }
     }
---
+++ README.md
@@ -0,0 +1,23 @@
+# Better chat
+A new and improved reworked chat
+
+<details>
+<summary>Previews</summary>
+
+<img src="https://i.imgur.com/pQU3326.png" alt="drawing" width="500"/>
+<img src="https://i.imgur.com/UZY8eVI.jpeg" alt="drawing" width="700"/>
+
+</details>
+
+## Features
+
+- Fully customizable chat window
+- Use enter to open and close the window
+- Use up and down arrow to scroll through chat history
+- If you want to open the vanilla chat windows use `left-shift` + `Enter`
+- This mod can fully be used clientside
+  
+### Known issues
+
+- If you type to the max character limit and re-open the chat the text becomes invisible
+    - How to fix: remove all text and close and re-open the chat window
